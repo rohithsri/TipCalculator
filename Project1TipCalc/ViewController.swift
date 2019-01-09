3ddd1039bf0c73fc74a3053bb78625a4bf944fa8
Rohith Sridharan <rohith@rohiths-macbook-pro.local>
2019-01-08 22:48:23 -0500

    Initial Commit

---
+++ Project1TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1TipCalc
+//
+//  Created by Rohith Sridharan on 1/8/19.
+//  Copyright © 2019 Rohith Sridharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
